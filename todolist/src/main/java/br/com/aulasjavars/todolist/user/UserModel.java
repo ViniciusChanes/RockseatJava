37663e73d1f6dfabb574fd91636b583bd573e6d1
ViniciusChanes <62079989+ViniciusChanes@users.noreply.github.com>
2023-10-15 11:26:47 -0300

    Dockerfiles
    teste

---
+++ todolist/src/main/java/br/com/aulasjavars/todolist/user/UserModel.java
@@ -0,0 +1,35 @@
+package br.com.aulasjavars.todolist.user;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    
+    @Column(name = "nome")
+    private String name;
+    
+    @Column(name = "senha")
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
---
+++ todolist/src/main/java/br/com/aulasjavars/todolist/user/iUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.aulasjavars.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface iUserRepository extends JpaRepository<UserModel, UUID>{
+    
+    UserModel findByUsername(String username);
+
+}
